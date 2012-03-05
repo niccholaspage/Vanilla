83849fdd80cd586f5ee3aa28df30eb68cc8d0b95
Nicholas Nassar <niccholaspage@gmail.com>
2012-03-04 19:54:03 -0500

    Start of the recode (No functionality yet)
    Multilanguage support & config handler is done, with conversion of old
    configurations

---
+++ src/com/niccholaspage/Vanilla/ConfigHandler.java
@@ -0,0 +1,124 @@
+package com.niccholaspage.Vanilla;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+public class ConfigHandler {
+	private final File configFile;
+	
+	private final File phrasesFile;
+	
+	private final YamlConfiguration config;
+	
+	private Set<String> hiddenPlugins;
+	
+	private boolean hidePluginsCommand;
+	
+	private boolean hideVersionCommand;
+	
+	public ConfigHandler(File configFile, File phrasesFile){
+		this.configFile = configFile;
+		
+		this.phrasesFile = phrasesFile;
+		
+		this.config = YamlConfiguration.loadConfiguration(configFile);
+		
+		load();
+	}
+	
+	public void load(){
+		config.setDefaults(getDefaultConfig());
+		
+		config.options().copyDefaults(true);
+		
+		//Converting of old config
+		ConfigurationSection VanillaSection = config.getConfigurationSection("Vanilla");
+		
+		if (VanillaSection != null){
+			for (String key : VanillaSection.getKeys(false)){
+				config.set(key, VanillaSection.get(key));
+			}
+			
+			config.set("Vanilla", null);
+		}
+		
+		//Converts old hidden plugins
+		if (config.isString("hiddenplugins")){
+			String[] oldHiddenPlugins = config.getString("hiddenplugins").split(",");
+			
+			List<String> newHiddenPlugins = new ArrayList<String>();
+			
+			for (String oldHiddenPlugin : oldHiddenPlugins){
+				newHiddenPlugins.add(oldHiddenPlugin);
+			}
+			
+			config.set("hiddenplugins", newHiddenPlugins);
+		}
+		
+		//Convert hideplugincommand boolean to hidepluginscommand
+		if (config.isBoolean("hideplugincommand")){
+			config.set("hidepluginscommand", config.getBoolean("hideplugincommand"));
+		}
+		
+		try {
+			config.save(configFile);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		hiddenPlugins = new HashSet<String>(config.getStringList("hiddenplugins"));
+		
+		hidePluginsCommand = config.getBoolean("hidepluginscommand");
+		
+		hideVersionCommand = config.getBoolean("hideversioncommand");
+		
+		for (Phrase phrase : Phrase.values()){
+			phrase.reset();
+		}
+		
+		if (!phrasesFile.exists()){
+			return;
+		}
+		
+		YamlConfiguration phrasesConfig = YamlConfiguration.loadConfiguration(phrasesFile);
+		
+		Set<String> keys = phrasesConfig.getKeys(false);
+		
+		for (Phrase phrase : Phrase.values()){
+			String phraseConfigName = phrase.getConfigName();
+			
+			if (keys.contains(phraseConfigName)){
+				phrase.setMessage(phrasesConfig.getString(phraseConfigName));
+			}
+		}
+	}
+	
+	private YamlConfiguration getDefaultConfig(){
+		YamlConfiguration defaultConfig = new YamlConfiguration();
+		
+		defaultConfig.set("hiddenplugins", "");
+		defaultConfig.set("hidepluginscommand", false);
+		defaultConfig.set("hideversioncommand", false);
+		
+		return defaultConfig;
+	}
+	
+	public Set<String> getHiddenPlugins(){
+		return hiddenPlugins;
+	}
+	
+	public boolean isHidingPluginsCommand(){
+		return hidePluginsCommand;
+	}
+	
+	public boolean isHidingVersionCommand(){
+		return hideVersionCommand;
+	}
+}
---
+++ src/com/niccholaspage/Vanilla/Phrase.java
@@ -0,0 +1,45 @@
+package com.niccholaspage.Vanilla;
+
+public enum Phrase {
+	NCHAT_COMMAND_CREDIT("Vanilla $1 by niccholaspage"),
+	NCHAT_CONFIG_RELOAD_HOW_TO("Type /$0 reload to reload the configuration"),
+	NCHAT_CONFIG_RELOADED("The Vanilla configuration has been reloaded.");
+
+	private String defaultMessage;
+	
+	private String message;
+
+	private Phrase(String defaultMessage){
+		this.defaultMessage = defaultMessage;
+		
+		message = defaultMessage + "";
+	}
+
+	public void setMessage(String message){
+		this.message = message;
+	}
+
+	private String getMessage(){
+		return message;
+	}
+	
+	public void reset(){
+		message = defaultMessage + "";
+	}
+	
+	public String getConfigName(){
+		return name().toLowerCase();
+	}
+	
+	public String parse(String... params){
+		String parsedMessage = getMessage();
+
+		if (params != null){
+			for (int i = 0; i < params.length; i++){
+				parsedMessage = parsedMessage.replace("$" + (i + 1), params[i]);
+			}
+		}
+
+		return parsedMessage;
+	}
+}
